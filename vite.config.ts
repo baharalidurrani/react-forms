8848cf63390e416e93056e4c3752945c79c227c0
baharalidurrani <baharalidurrani@gmail.com>
2023-08-21 12:29:46 +0500

    github pages yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://baharalidurrani.github.io/react-forms",
   plugins: [react(), basicSsl()],
 });
