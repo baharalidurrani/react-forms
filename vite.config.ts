4e64f9e1d09e93d158917d645868619acb9e4737
baharalidurrani <baharalidurrani@gmail.com>
2023-08-29 16:24:44 +0500

    Bundle Optimization Fixed?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://baharalidurrani.github.io/react-forms",
+  base: 'https://baharalidurrani.github.io/react-forms',
   plugins: [react(), basicSsl()],
   optimizeDeps: {
-    include: ["@mui/material/Tooltip", "@emotion/styled"],
+    include: ['@mui/material/Tooltip', '@emotion/styled', '@mui/material/Unstable_Grid2'],
   },
 });
